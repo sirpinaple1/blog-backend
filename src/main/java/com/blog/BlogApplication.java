3cba310ddf96bac7dbe4acec844767498d044953
sirpianle1 <panlin418@gmail.com>
2026-03-09 18:42:31 +0800

    feat: 准备自动建表

--- src/main/java/com/blog/BlogApplication.java
+++ src/main/java/com/blog/BlogApplication.java
@@ -1,12 +1,15 @@
 package com.blog;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @MapperScan("com.blog.mapper")
 public class BlogApplication {
+    
     public static void main(String[] args) {
         SpringApplication.run(BlogApplication.class, args);
     }
